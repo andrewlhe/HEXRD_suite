3efae7979fc58bb773d367001de7fab854c368ef
Lewei He <lh3rt@virginia.edu>
2022-11-08 12:59:38 -0500

    Add new pole figure functions

--- waxs_pole_figure_new.py
+++ waxs_pole_figure_new.py
@@ -1,9 +1,5 @@
 import csv
 import os
-import math
-import random
-import string
-
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -14,8 +10,6 @@ import numpy as np
 import pandas as pd
 
 # Function to save csv
-
-
 def save_csv(save_file_path, current_file_name, data_type, data):
 
     if not os.path.exists(save_file_path):
@@ -26,25 +20,6 @@ def save_csv(save_file_path, current_file_name, data_type, data):
     with open(csv_file_path, "w", newline="") as output_file:
         csv_writer = csv.writer(output_file)
         csv_writer.writerows(data)
-
-
-def make_plot(matrix, output_folder, current_file_name, note, lower_bound, upper_bound):
-
-    if not os.path.exists(output_folder):
-        os.mkdir(output_folder)
-
-    fig, ax = plt.subplots(1, 1)
-    img = ax.imshow(matrix, interpolation='none',
-                    vmin=lower_bound, vmax=upper_bound, extent=[0, 90, 0, 360], cmap='bwr')
-    ax.set_xticks([0, 30, 60, 90])
-    ax.set_yticks([0, 90, 180, 270, 360])
-    fig.colorbar(img, orientation='vertical')
-    ax.set_aspect('auto')
-
-    plt.savefig(str(output_folder + current_file_name + note),
-                bbox_inches='tight')
-    plt.clf()
-    plt.close(fig)
 
 
 def extract_parameter(peak_index, data_path, debug_path):
@@ -62,7 +37,7 @@ def extract_parameter(peak_index, data_path, debug_path):
         center = centers.loc[0].iat[peak_index]
         center_list.append(center)
 
-    print(len(center_list))
+    # print(len(center_list))
     return center_list
 
 
@@ -91,8 +66,57 @@ def compose_strains(tth, eta, omega, scans, peak, sample, debug_path):
 
     return strain
 
+def compose_intensity(intensity, eta, omega, scans, peak, sample, debug_path):
+    if not os.path.exists(debug_path):
+        os.mkdir(debug_path)
 
-def make_plots(image_path, sample, scans, peak, data, lower_bound, upper_bound):
+    relative_intensity = np.zeros([scans, omega, eta])
+    intensity_mat = np.zeros([scans, omega, eta])
+    
+    index = 0
+    for i in range(scans):
+        for j in range(omega):
+            for k in range(eta):
+                # print(index)
+                intensity_mat[i, j, k] = intensity[index]
+                index += 1
+    
+    for i in range(scans):
+        for j in range(omega):
+            for k in range(eta):             
+                relative_intensity[i, j, k] = intensity_mat[i, j, k]/intensity_mat[0, 0, 0]
+        # Code to save CSV files
+        data_type = "intensity_" + str(i).rjust(2, '0') + "_"+ str(peak).rjust(2, '0')
+        save_csv(debug_path, sample, data_type, relative_intensity[i, :, :])
+
+    return relative_intensity
+
+def compose_fwhm(fwhm, eta, omega, scans, peak, sample, debug_path):
+    if not os.path.exists(debug_path):
+        os.mkdir(debug_path)
+
+    relative_fwhm = np.zeros([scans, omega, eta])
+    fwhm_mat = np.zeros([scans, omega, eta])
+    
+    index = 0
+    for i in range(scans):
+        for j in range(omega):
+            for k in range(eta):
+                # print(index)
+                fwhm_mat[i, j, k] = fwhm[index]
+                index += 1
+    
+    for i in range(scans):
+        for j in range(omega):
+            for k in range(eta):             
+                relative_fwhm[i, j, k] = fwhm_mat[i, j, k]/fwhm_mat[0, j, k]
+        # Code to save CSV files
+        data_type = "fwhm_" + str(i).rjust(2, '0') + "_"+ str(peak).rjust(2, '0')
+        save_csv(debug_path, sample, data_type, relative_fwhm[i, :, :])
+
+    return relative_fwhm
+
+def make_plots(image_path, sample, type, scans, peak, data, lower_bound, upper_bound):
     if not os.path.exists(image_path):
         os.mkdir(image_path)
 
@@ -104,7 +128,7 @@ def make_plots(image_path, sample, scans, peak, data, lower_bound, upper_bound):
         ax.set_yticks([0, 30, 60, 90])
         fig.colorbar(img, orientation='vertical')
         ax.set_aspect('auto')
-        path_comp = str(image_path) + sample + "_" + str(peak).rjust(2, '0') + "_" + str(i).rjust(2, '0')
+        path_comp = str(image_path) + sample + "_" + type + "_" + str(peak).rjust(2, '0') + "_" + str(i).rjust(2, '0')
         plt.savefig(path_comp, bbox_inches='tight')
         plt.clf()
         plt.close(fig)
@@ -130,13 +154,15 @@ def pole_figure_ti(type, lb, ub, peaks, sample, scans, data_path, image_path, de
         if type == "strain":
             strain = compose_strains(
                 parameter, eta, omega, scans, peak, sample, debug_folder_path)
-            make_plots(image_folder_path, sample, scans, peak, strain, lb, ub)
-        # elif type == "intensity":
-        #     polt_pole_figure_intensity(lower_bound, upper_bound, peak, sample,
-        #                                data_path, debug_path, image_folder_path)
-        # elif type == "fwhm":
-        #     polt_pole_figure_fwhm(lower_bound, upper_bound, peak, sample,
-        #                           data_path, debug_path, image_folder_path)
+            make_plots(image_folder_path, sample, type, scans, peak, strain, lb, ub)
+        elif type == "intensity":
+            intensity = compose_intensity(
+                parameter, eta, omega, scans, peak, sample, debug_folder_path)
+            make_plots(image_folder_path, sample, type, scans, peak, intensity, lb, ub)
+        elif type == "fwhm":
+            fwhm = compose_fwhm(
+                parameter, eta, omega, scans, peak, sample, debug_folder_path)
+            make_plots(image_folder_path, sample, type, scans, peak, fwhm, lb, ub)
         else:
             print("error")
             quit()
@@ -144,17 +170,17 @@ def pole_figure_ti(type, lb, ub, peaks, sample, scans, data_path, image_path, de
 
 def main():
     # # For CHESS 2022-2 Ti-TiB samples
-    pole_figure_ti("strain", -.01, .01, np.arange(0, 6), "Ti-TiB-4v", 13, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-4v-eta-all\results\center",
+    pole_figure_ti("strain", -.02, .02, np.arange(0, 6), "Ti-TiB-4v", 13, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-4v-eta-all\results\center",
                    r"Y:\CHESS\ID3A_2022-2\polefigures\ti-tib-4v", r"Y:\CHESS\ID3A_2022-2\debug\ti-tib-4v")
-    pole_figure_ti("strain", -.01, .01, np.arange(0, 6), "Ti-TiB-4p", 38, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-4p-eta-all\results\center",
+    pole_figure_ti("strain", -.02, .02, np.arange(0, 6), "Ti-TiB-4p", 38, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-4p-eta-all\results\center",
                    r"Y:\CHESS\ID3A_2022-2\polefigures\ti-tib-4p", r"Y:\CHESS\ID3A_2022-2\debug\ti-tib-4p")
-    pole_figure_ti("strain", -.01, .01, np.arange(0, 6), "Ti-TiB-3", 15, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-3-eta-all\results\center",
+    pole_figure_ti("strain", -.02, .02, np.arange(0, 6), "Ti-TiB-3", 15, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-3-eta-all\results\center",
                    r"Y:\CHESS\ID3A_2022-2\polefigures\ti-tib-3", r"Y:\CHESS\ID3A_2022-2\debug\ti-tib-3")
-    pole_figure_ti("strain", -.01, .01, np.arange(0, 6), "Ti-TiB-2b", 27, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-2b-eta-all\results\center",
+    pole_figure_ti("strain", -.02, .02, np.arange(0, 6), "Ti-TiB-2b", 27, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-2b-eta-all\results\center",
                    r"Y:\CHESS\ID3A_2022-2\polefigures\ti-tib-2b", r"Y:\CHESS\ID3A_2022-2\debug\ti-tib-2b")
-    pole_figure_ti("strain", -.01, .01, np.arange(0, 6), "Ti-TiB-2", 24, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-2-eta-all\results\center",
+    pole_figure_ti("strain", -.02, .02, np.arange(0, 6), "Ti-TiB-2", 24, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-2-eta-all\results\center",
                    r"Y:\CHESS\ID3A_2022-2\polefigures\ti-tib-2", r"Y:\CHESS\ID3A_2022-2\debug\ti-tib-2")
-    pole_figure_ti("strain", -.01, .01, np.arange(0, 6), "Ti-TiB-1", 36, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-1-eta-all\results\center",
+    pole_figure_ti("strain", -.02, .02, np.arange(0, 6), "Ti-TiB-1", 36, r"Y:\CHESS\ID3A_2022-2\lineouts\ti-tib-1-eta-all\results\center",
                    r"Y:\CHESS\ID3A_2022-2\polefigures\ti-tib-1", r"Y:\CHESS\ID3A_2022-2\debug\ti-tib-1")
 
 if __name__ == "__main__":
